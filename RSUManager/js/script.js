55992f6b2824d7096cb7ce8a33460e8382595c1b
Yi <argon.zhou@connect.polyu.hk>
2017-02-15 17:06:59 +0800

    separate files

--- RSUManager/js/script.js
+++ RSUManager/js/script.js
@@ -6,6 +6,33 @@ function randomStatus() {
 }
 
 $(function() {
+
+  function bindDetailLink(e) {
+    $(".table-container").hide();
+    $(".main-container").show();
+    var $ele = $(e.target);
+    dev_id = $ele.text();
+    $(".detail-wrapper").empty().prepend(`
+        <h1>`+dev_id+`</h1>
+        <div class="detail-attr-wrapper">
+          <ul class="detail-attr-list">
+            <li><span class="attr"><strong>Attribute 1:</strong></span> <span class="val">`+dev_id+`.Value1<span></li>
+            <li><span class="attr"><strong>Attr 3:</strong></span> <span class="val">`+dev_id+`.Value2</span></li>
+            <li><span class="attr"><strong>Attrib 2:</strong></span> <span class="val">`+dev_id+`.Value3</span></li>
+            <li><span class="attr"><strong>Attri 4:</strong></span> <span class="val">`+dev_id+`.Value4</span></li>
+            <li><span class="attr"><strong>Attribute 5:</strong></span> <span class="val">`+dev_id+`.Value5</span></li>
+          </ul>
+        </div>
+        <div class="location-map"><img src="img/map_dummy.png" class="map" alt=""></div>
+    `);
+  }
+
+  $("#main-header").load("header.html");
+  $("#main-sidebar").load("sidebar.html");
+  $("#main-sidebar, .rsu-table").delegate(".detail-link", "click", bindDetailLink);
+  $(".nav-tabs").load("nav_tabs.html");
+
+
   var resizerActive = false;
   $(".resizer").mousedown(function(e) {
     resizerActive = true;
@@ -19,64 +46,11 @@ $(function() {
     if (e.pageX > 180 && e.pageX < 600)
     {
       $(".resizer").css("left", e.pageX);
-      $(".sidebar-list").css("width", e.pageX);
       $("section.sidebar").css("width", e.pageX);
       $(".main-container, .table-container").css("padding-left", e.pageX);
       $(".nav-tabs").css("left", e.pageX);
     }
   });
-  for (var ii = 0; ii < 64; ++ii)
-    $("#rsu-list").append(`
-    <a href="#rsu`+ii+`" class="sidebar-item detail-link">RSU`+ii+`</a>
-    `);
-
-  for (var ii=0; ii < 33; ++ii)
-    $("#obu-list").append(`
-      <a href="#obu`+ii+`" class="sidebar-item detail-link">OBU`+ii+`</a>
-    `);
-
-  var $items = $(".sidebar-item.expandable");
-  $items.click(function(e) {
-    var $ele = $(e.target);
-    $ele.next().toggle();
-    $ele.toggleClass("expanded");
-    $ele.children(".list-arrow").toggleClass("glyphicon-triangle-bottom");
-  });
-  var $subitems = $(".sidebar-sublist .sidebar-item");
-  var len = $subitems.length;
-  $("#search-input").on("input", function(e) {
-    // search logic
-    if (this.value) 
-    {
-      $(".sidebar-item.expandable").hide();
-      $("#search-clear-btn").show();
-      $(".sidebar-sublist").show();
-    }
-    else 
-    {
-      $(".sidebar-item.expandable").show();
-      $(".sidebar-sublist").hide();
-      $("#search-clear-btn").hide();
-    }
-
-    for (var ii = 0; ii < len; ++ii)
-    {
-      var ele = $subitems[ii];
-      if (ele.text.toUpperCase().indexOf(this.value.toUpperCase()) < 0) ele.style.display = "none";
-      else ele.style.display = "";
-    }
-  });
-  $("#search-clear-btn").click(function() {
-    // clear search field
-    $("#search-input").val("");
-    $(".sidebar-item.expandable").show();
-    $(".sidebar-sublist").hide();
-    $("#search-clear-btn").hide();
-    for (var ii = 0; ii < len; ++ii)
-      $subitems[ii].style.display = "";
-    $(".bookmark-item.active").click();
-  });
-
   $(".rsu-table thead").append(`
     <tr>
       <th><a href="#">ID</a></th>
@@ -111,27 +85,6 @@ $(function() {
     );
   }
 
-  $(".detail-link").click(function (e) {
-    $(".table-container").hide();
-    $(".main-container").show();
-    var $ele = $(e.target);
-    dev_id = $ele.text();
-    $(".detail-wrapper").empty().prepend(`
-        <h1>`+dev_id+`</h1>
-        <div class="detail-attr-wrapper">
-          <ul class="detail-attr-list">
-            <li><span class="attr"><strong>Attribute 1:</strong></span> <span class="val">`+dev_id+`.Value1<span></li>
-            <li><span class="attr"><strong>Attr 3:</strong></span> <span class="val">`+dev_id+`.Value2</span></li>
-            <li><span class="attr"><strong>Attrib 2:</strong></span> <span class="val">`+dev_id+`.Value3</span></li>
-            <li><span class="attr"><strong>Attri 4:</strong></span> <span class="val">`+dev_id+`.Value4</span></li>
-            <li><span class="attr"><strong>Attribute 5:</strong></span> <span class="val">`+dev_id+`.Value5</span></li>
-          </ul>
-        </div>
-        <div class="location-map"><img src="img/map_dummy.png" class="map" alt=""></div>
-    `);
-  });
-
-
   $uploadForm = $("form.upload-form");
 
   $("button.upgrade, button.downgrade").click(function() {
@@ -143,45 +96,6 @@ $(function() {
     console.log("here");
     $uploadForm.slideToggle(200);
   });
-
-  function nav_click(_this, nav_name) {
-    $("."+nav_name+"-item.active").removeClass("active");
-    console.log(_this);
-    $(_this).addClass("active");
-  }
-
-  $(".bookmark-item").click(function() {nav_click(this, "bookmark")});
-
-  $("#bookmark-overview").click(function() {
-    $("#rsu-list-expander").show();
-    $("#obu-list-expander").show();
-    if ($("#rsu-list-expander").hasClass("expanded")) {
-      $("#rsu-list").show();
-    }
-    if ($("#obu-list-expander").hasClass("expanded")) {
-      $("#obu-list").show();
-    }
-  });
-
-  $("#bookmark-rsu").click(function() {
-    $("#obu-list").hide();
-    $("#obu-list-expander").hide();
-    $("#rsu-list-expander").show();
-    if ($("#rsu-list-expander").hasClass("expanded")) {
-      $("#rsu-list").show();
-    }
-  });
-
-  $("#bookmark-obu").click(function() {
-    $("#rsu-list").hide();
-    $("#rsu-list-expander").hide();
-    $("#obu-list-expander").show();
-    if ($("#obu-list-expander").hasClass("expanded")) {
-      $("#obu-list").show();
-    }
-  }); 
-
-  $(".tab-item").click(function() {nav_click(this, "tab")});
 
 });
 
